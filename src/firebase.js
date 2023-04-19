d7243bc27f154f3f6d5cc1bf8cef34097cf7ab45
Stephen Castaneda <stephencastaneda@Stephens-MacBook-Pro.local>
2023-04-18 18:33:04 -0700

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAKPz1ASUgqnG-opCK6_UwyVZX_T6b4dyY",
+    authDomain: "netflix-clone-c3b4d.firebaseapp.com",
+    projectId: "netflix-clone-c3b4d",
+    storageBucket: "netflix-clone-c3b4d.appspot.com",
+    messagingSenderId: "385343477340",
+    appId: "1:385343477340:web:c6c64c31715cd76995f899"
+  };
